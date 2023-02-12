213da7e47b29760f7ba91c10acb256ea2a3744ed
Ilya <kotyrov.1310@gmail.com>
2023-02-12 13:44:34 +0100

    fix game field logic

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+
+let GameField = (function () {
+	const _field = [
+		['', '', ''],
+		['', '', ''],
+		['', '', ''],
+	];
+
+	function reset() {
+		for (let i = 0; i <= 2; ++i) {
+			for (let j = 0; j <= 2; ++j) {
+				_field[i][j] = '';
+			}
+		}
+	}
+
+	function putX(i, j) {
+		return _putMark(i, j, 'x');
+	}
+
+	function putO(i, j) {
+		return _putMark(i, j, 'o');
+	}
+
+	function getMark(i, j) {
+		return _field[i][j];
+	}
+
+	function _putMark(i, j, mark) {
+		if (i >= 0 && i <= 2 && j >= 0 && j <= 2 && _field[i][j] === '') {
+			_field[i][j] = mark;
+			return _checkPosition(i, j);
+		}
+
+		throw GameError.InvalidCoordinates(i, j);
+	}
+
+	function _checkRow(row) {
+		if (_field[row][0] === '') {
+			return false;
+		}
+		return _field[row][0] === _field[row][1] && _field[row][0] === _field[row][2];
+	}
+
+	function _checkColumn(col) {
+		if (_field[0][col] === '') {
+			return false;
+		}
+		return _field[0][col] === _field[1][col] && _field[0][col] === _field[2][col];
+	}
+
+	function _checkDiagonal() {
+		if (_field[1][1] === '') {
+			return false;
+		}
+		return (
+			(_field[1][1] === _field[0][0] && _field[1][1] === _field[2][2]) ||
+			(_field[1][1] === _field[0][2] && _field[1][1] === _field[2][0])
+		);
+	}
+
+	function _checkPosition(i, j) {
+		if (!((i + j) % 2)) {
+			// check corners and center
+			if (_checkDiagonal()) {
+				return true;
+			}
+		}
+		// check edges
+		return _checkRow(i) || _checkColumn(j);
+	}
+
+	return {
+		getMark,
+		putO,
+		putX,
+		reset,
+	};
+})();
+
+class GameError extends Error {
+	constructor(message) {
+		super(message);
+	}
+
+	static InvalidCoordinates(i, j) {
+		return new GameError(`Can't place mark on coordinates: ${i} ${j}`);
+	}
+}
